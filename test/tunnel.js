6ba05aa8404414d9b1245a8704f70254bd432a65
Sergey Chikuyonok <serge.che@gmail.com>
2015-07-14 00:04:44 +0300

    Added tunnel cluster controller

---
+++ test/tunnel.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var assert = require('assert');
+var tunnelController = require('../lib/tunnel');
+
+// No need to check full Remote View connectivity,
+// simply check that controller is properly instantiated and emits messages
+
+describe('Tunnel Cluster controller', function() {
+	it('create and emit messages', function(done) {
+		var clusterCreated = false;
+		var updates = [];
+
+		var tc = tunnelController()
+		.on('update', function(list) {
+			updates.push(list);
+		})
+		.on('clusterCreate', function(cluster) {
+			clusterCreated = true;
+		})
+		.on('clusterDestroy', function(cluster) {
+			setTimeout(function() {
+				assert(clusterCreated);
+				assert.equal(updates.length, 2);
+				assert.equal(updates[0][0].publicId, 'rv-test.livestyle.io');
+				assert.equal(updates[0][0].state, 'idle');
+
+				// the second update is empty list because cluster was destroyed
+				assert.deepEqual(updates[1], []);				
+				done();
+			}, 20);
+		});
+
+		tc.create({
+			publicId: 'rv-test.livestyle.io',
+			localSite: 'http://localhost:8901',
+			url: 'http://localhost:8902/fake-session',
+			maxConnections: 2,
+			retryCount: 2,
+			retryDelay: 100,
+		});
+	});
+});
